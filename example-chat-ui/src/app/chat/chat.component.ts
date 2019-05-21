2c11e612e9fad50a6800900c166c6425a0dd64a6
lakinduakash <lakinduakash@gmail.com>
2019-05-22 03:42:51 +0530

    update ui

--- example-chat-ui/src/app/chat/chat.component.ts
+++ example-chat-ui/src/app/chat/chat.component.ts
@@ -9,7 +9,12 @@ import {SocketService} from "../socket.service";
 export class ChatComponent implements OnInit {
 
   msg
-  history:MessageEvent[]=[]
+  history:MessageBody[]=[]
+  connectedList:string[]=[]
+  toUser:string
+
+  counter=0
+  myId:string
 
   constructor(public wsService:SocketService) { }
 
@@ -26,8 +31,21 @@ export class ChatComponent implements OnInit {
     };
 
     socket.onmessage = msg => {
-      console.log(msg);
-      this.history.push(msg)
+      console.log(msg)
+      this.counter++
+      let data =JSON.parse(msg.data) as SocketMessage
+
+      if(data.type ==1)
+        this.history.push(data.body)
+      else if(data.type ==2) {
+        if(this.counter==1)
+          this.myId=data.body.message
+        else
+          this.connectedList.push(data.body.message)
+      }
+      else if(data.type ==3)
+        this.connectedList=this.connectedList.filter(val=>val!= data.body.message)
+
     };
 
     socket.onclose = event => {
@@ -41,8 +59,23 @@ export class ChatComponent implements OnInit {
 
   sendMsg(msg) {
     let socket = this.wsService.ws
-    console.log("sending msg: ", msg);
-    socket.send(msg);
+    let body:MessageBody={
+      from:this.myId,
+      to:this.toUser,
+      message:msg
+    }
+    socket.send(JSON.stringify(body));
   };
 
+}
+
+export interface MessageBody {
+  from
+  to
+  message
+}
+
+export interface SocketMessage {
+  type
+  body:MessageBody
 }
